08b5ef0bc916855928428eaba579ee2de9d21a38
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-12-20 17:12:55 +0100

    Implement constructor with string

--- src/main/java/com/codecool/krk/oni/exception/NoSuchShowroomException.java
+++ src/main/java/com/codecool/krk/oni/exception/NoSuchShowroomException.java
@@ -1,4 +1,7 @@
 package com.codecool.krk.oni.exception;
 
 public class NoSuchShowroomException extends Exception {
+    public NoSuchShowroomException(String message) {
+        super(message);
+    }
 }
